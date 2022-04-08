58b3a0ac41e3a523b82d2e3db992833a0149aa7c
hokomili <hokomili@gmail.com>
2022-04-09 04:41:52 +0800

    merge

--- Assets/Scripts/cpController.cs
+++ Assets/Scripts/cpController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class cpController : MonoBehaviour
 {
     public GameObject pb;
+    public int selfindex;
     public bool alive;
+    private GameObject effec;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +19,22 @@ public class cpController : MonoBehaviour
     {
         if (!alive)
         {
-            Instantiate(pb, this.gameObject.transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
+            effec=Instantiate(pb, this.gameObject.transform.position, Quaternion.identity);
+            Destroy(effec,2000.0f);
+            alive=true;
+            GetComponentInParent<CheckpointController>().Checkpoints[selfindex].gameObject.SetActive(false);
         }
     }
     void OnTriggerEnter(Collider collision){
-    if(collision.gameObject.tag == "Player"){
-        alive=false;
+        Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.tag == "Player"){
+            alive=false;
+            if(selfindex+1<GetComponentInParent<CheckpointController>().Checkpoints.Length){
+                GetComponentInParent<CheckpointController>().Checkpoints[selfindex+1].SetActive(true);
+            }
+            else{
+                GetComponentInParent<CheckpointController>().Checkpoints[0].SetActive(true);
+            }
+        }
     }
- }
 }
